ffb6b9dbba59a7730ba89c54da858454b8fedf9c
valentinvion <valentinvion@yahoo.fr>
2017-09-25 11:30:12 +0200

    1er version the loop

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
-<?php include('header.php');?>
-<h2>Hello world</h2>
-<?php include('footer.php');?>
+<?php get_header('header.php');?>
+
+<?php if (have_posts()):while(have_posts()):the_post();?>
+  <h2><?php the_title();?></h2>
+  <p><?php the_content();?></p>
+<?php endwhile; endif;?>
+<?php get_footer('footer.php');?>
